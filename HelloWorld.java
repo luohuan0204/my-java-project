5a18a02dc0f33932f7e42973fe684127cc1b38c1
Luo Huan <61372310+luohuan0204@users.noreply.github.com>
2025-02-24 16:55:33 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,4 +2,12 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, GitHub!");
     }
+
+    public void sayHello() {
+        System.out.println("Hello again!");
+    }
+
+    public int addNumbers(int a, int b) {
+        return a + b;
+    }
 }
